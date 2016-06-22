f4d27d60086fe1ed37dee5ea6dbd22c15bbc0922
Ivan Hernandez Ortega <ivanherdezort@gmail.com>
2016-06-22 16:54:02 -0500

    SchedulerTask cambia status si la fecha es anterior

---
+++ src/com/boe/apps/util/SchedulerTask.java
@@ -0,0 +1,24 @@
+package com.boe.apps.util;
+
+import java.util.TimerTask;
+
+import com.boe.apps.data.LAOData;
+import com.boe.apps.data.LAOImplementation;
+
+public class SchedulerTask extends TimerTask {
+	
+	private LAOData obj;
+	
+	public SchedulerTask(){
+		obj = new LAOImplementation();
+	}
+	public void run() {
+		try {
+			obj.changeStatus();
+			System.out.println("Working...");
+			} 
+		catch (Exception ex) {
+	        System.out.println("error running thread " + ex.getMessage());
+		}
+   }
+}
